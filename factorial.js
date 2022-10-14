b0fe2747def769cd9b59b330e975c91f9fb9451d
hanticoy <hugo.anticoy.anticoy@gmail.com>
2022-10-13 23:10:24 -0300

    actualiza ejecucion

---
+++ factorial.js
@@ -0,0 +1,6 @@
+var factor = 1;
+
+for (var i = 1; i <= 12; i++) {
+    factor = factor * i;
+}
+console.log('Total:' + factor);
---
+++ sigma.js
@@ -0,0 +1,7 @@
+
+var suma = 0;
+
+for (var i = 1; i <= 100; i++) {
+    suma = suma +i;
+}
+console.log('Total:' + suma);
